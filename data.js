28560b4cebb7f44f5e6a890153ef9d2038502d3b
Pangzidao <nicolasyvonlejeune@gmail.com>
2023-03-22 21:20:31 +0100

    com

---
+++ data.js
@@ -0,0 +1,51 @@
+console.log("data")
+
+const formulaire = document.getElementById("formulaire")
+const data = document.getElementById("data")
+
+formulaire && formulaire.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(e)
+})
+
+class Event {
+    constructor(nom, description, image, date) {
+        this.nom = nom
+        this.description = description
+        this.image = image
+        this.date = date
+    }
+}
+
+
+
+export let events = [
+    {
+        "nom" : "tournoi",
+        "description" : "Des matchs intenses et une ambiance conviviale : Retour sur le tournoi de padel de notre club",
+        "image" : "./images/le-padel-tennis-est-un-sport-tres-spectaculaire-ici-sur-le_5266766_1000x526.jpg",
+        "date": "15/09/2023"
+    },
+    {
+        "nom" : "nouveau coach",
+        "description" : "Renfort de choix pour notre club de padel : Présentation de notre nouveau coach !",
+        "image" : "./images/Coach_&_trainer.jpg",
+        "date": "8/08/2023"
+    },
+    {
+        "nom" : "championnat de France",
+        "description" : "Victoire de notre joueuse au championnat de France",
+        "image" : "./images/JUGADORA_DE_PADEL.jpg",
+        "date": "18/07/2023"
+    }
+]
+
+events.push(new Event("classe", "fnioazhochaoifio", "niqevninef", "4 juin"))
+events.pop()
+
+
+if (data){
+    events.forEach((e) => {
+        data.innerHTML += JSON.stringify(e)
+    })
+}
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log("hello")
 
+import {events} from "./data.js"
+
 const colors = {
     "white": "#fff",
     "green": "#b9dbb4",
@@ -9,16 +11,95 @@ const colors = {
 }
 
 const header = document.getElementById("header")
+const headerLogo = document.getElementById("header-logo")
 const headerLinks = document.querySelectorAll('header nav a')
+const sectionActualites = document.getElementById("section-actualites")
+const news = document.querySelectorAll("news")
+const leftButton = document.getElementById("news-left-button")
+const rightButton = document.getElementById("news-right-button")
 
-console.log(headerLinks)
+
+
+
+
+function init(){
+    console.log("initialize")
+    sectionActualites.innerHTML = carrousel(events)
+}
+
+init()
+
+
+
+console.log(events)
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 800){
+    if (window.scrollY > 1000){
         header.style.backgroundColor = colors.lightblue
-        header.style.height = "90px"
+        headerLogo.style.marginLeft = "120px"
+        header.style.height = "120px"
     }else{
         header.style.backgroundColor = "transparent"
-        header.style.height = "120px"
+        header.style.height = "160px"
+        headerLogo.style.marginLeft = "-160px"
     }
 })
+
+
+
+leftButton.onclick = () => {
+
+    let shiftingItems = events.shift()
+    events.push(shiftingItems)
+    console.log(events)
+    sectionActualites.innerHTML = carrousel(events)
+}
+
+rightButton.onclick = () => {
+
+    let shiftingItems = events.pop()
+    events.unshift(shiftingItems)
+    console.log(events)
+    sectionActualites.innerHTML = carrousel(events)
+}
+
+function carrousel(events){
+
+    let html = ""
+
+    /*
+    html = `
+        <div class="news">
+            <h3>${events[indexUn].nom}</h3>
+            <p>${events[indexUn].date}</p>
+        </div>
+        <div class="news">
+            <h3>${events[indexDeux].nom}</h3>
+            <p>${events[indexDeux].date}</p>
+        </div>
+        <div class="news">
+            <h3>${events[indexTrois].nom}</h3>
+            <p>${events[indexTrois].date}</p>
+        </div>
+    `
+    */
+
+    let eventsDisplay = events.slice(0,3)
+    console.log(eventsDisplay)
+    eventsDisplay.forEach((evenement) => {
+        html += 
+        `
+        <div class="news" style="background-image: url(${evenement.image}); 
+                                background-position:"center"
+                                >
+            <h3>${evenement.nom}</h3>
+            <p>${evenement.description}</p>
+            <p>${evenement.date}</p>
+        </div>
+        `
+    })
+
+    return html
+}
+
+
